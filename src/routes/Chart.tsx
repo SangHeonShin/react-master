c62c8f9bfe65a9cf9df569d0696cbfc125edde8b
SangHeonShin <nomadcoders@LAPTOP-A1U2NPS0>
2024-11-12 21:45:43 +0900

    #5.12 Price Chart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,4 +1,15 @@
-function Chart() {
+import { useQuery } from "@tanstack/react-query";
+import { fetchCoinHistory } from "../api";
+
+interface chartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: chartProps) {
+  const { isLoading, data } = useQuery({
+    queryKey: ["ohlcv", coinId],
+    queryFn: () => fetchCoinHistory(coinId),
+  });
   return <h1>Chart</h1>;
 }
 
