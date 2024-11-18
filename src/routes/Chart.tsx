cb606461997612e9bf9f161bc83a6b21874817d9
SangHeonShin <nomadcoders@LAPTOP-A1U2NPS0>
2024-11-18 23:17:46 +0900

    #5.13 Price Chart part Two

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,16 +1,75 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface Ihistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 
 interface chartProps {
   coinId: string;
 }
 
 function Chart({ coinId }: chartProps) {
-  const { isLoading, data } = useQuery({
+  const { isLoading, data } = useQuery<Ihistorical[]>({
     queryKey: ["ohlcv", coinId],
     queryFn: () => fetchCoinHistory(coinId),
   });
-  return <h1>Chart</h1>;
+  console.log(data?.map((price) => price.close));
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "price",
+              data: data?.map((price) => price.close) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            stroke: {
+              curve: "smooth",
+              width: 5,
+            },
+            grid: {
+              show: false,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: {
+                show: false,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
